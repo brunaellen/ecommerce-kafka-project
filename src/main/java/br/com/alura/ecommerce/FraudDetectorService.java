d86a97a57ecf7639098e72016359cf64593eef50
brunaellen <brunaellen.souza@hotmail.com>
2022-05-19 22:57:36 +0100

    refactoring consumer(FraudDetectorService)

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,17 +1,15 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
+        var fraudService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudService::Parse);
+        service.run();
+    }
+
+        /*var consumer = new KafkaConsumer<String, String>(properties());
         consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
 
         while(true) {
@@ -20,34 +18,26 @@ public class FraudDetectorService {
             if (!records.isEmpty()) {
                 System.out.println("The number of records found is : "+ records.count());
                 for (var record : records) {
-                    System.out.println("____________________________________");
-                    System.out.println("Processing new orders, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
                 }
                 continue;
             }
 
 
         }
-    }
+    }*/
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "" + UUID.randomUUID());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
-        return properties;
+    private void Parse(ConsumerRecord<String, String> record) {
+        System.out.println("____________________________________");
+        System.out.println("Processing new orders, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
     }
 }
